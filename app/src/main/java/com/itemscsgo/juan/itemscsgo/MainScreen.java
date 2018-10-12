fc0a5b8a06bdb1bfa3c58b4ad7c74907a8e0dc79
Potato211 <Juanmf98@hotmail.com>
2018-10-12 18:04:35 -0300

    Agregado "Onclick"

--- app/src/main/java/com/itemscsgo/juan/itemscsgo/MainScreen.java
+++ app/src/main/java/com/itemscsgo/juan/itemscsgo/MainScreen.java
@@ -2,12 +2,11 @@ package com.itemscsgo.juan.itemscsgo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainScreen extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
     }
-}
